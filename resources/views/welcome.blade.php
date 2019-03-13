9c44a4fb817ff2d5ea37d5559297848cf4e014fb
Meng Li <2426525782@qq.com.com>
2019-03-13 11:37:27 +0800

    ccc

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,6 +82,7 @@
             <div class="content">
                 <div class="title m-b-md">
                     :)Laravel(:
+					2019/3/13
                 </div>
 
                 <div class="links">
